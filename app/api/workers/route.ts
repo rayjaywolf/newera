1f16bbcb9e332bfb26157a066a8e96dc33dedf42
rayjay8 <jintarehaan@gmail.com>
2025-01-19 22:20:19 +0530

    deploy

--- app/api/workers/route.ts
+++ app/api/workers/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { WorkerType } from "@prisma/client";
 
 export async function POST(request: Request) {
