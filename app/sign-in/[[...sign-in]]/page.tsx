649f39736279961ea8130e2e97562b0055f00ef4
rayjay8 <jintarehaan@gmail.com>
2025-02-03 11:27:30 +0530

    sign in/up page enhancements

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -9,7 +9,6 @@ export default function SignInPage() {
             formButtonPrimary:
               "bg-primary text-primary-foreground hover:bg-primary/90",
             footerActionLink: "text-primary hover:text-primary/90",
-            card: "bg-white/[0.34] shadow-none border border-[rgba(0,0,0,0.08)]",
           },
         }}
         signUpUrl="/sign-up"
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,21 +1,19 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-    return (
-        <div className="min-h-screen w-full flex items-center justify-center">
-            <SignUp
-                appearance={{
-                    elements: {
-                        formButtonPrimary:
-                            "bg-primary text-primary-foreground hover:bg-primary/90",
-                        footerActionLink:
-                            "text-primary hover:text-primary/90",
-                        card: "bg-white/[0.34] shadow-none border border-[rgba(0,0,0,0.08)]",
-                    },
-                }}
-                signInUrl="/sign-in"
-                fallbackRedirectUrl='/new-user'
-            />
-        </div>
-    );
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <SignUp
+        appearance={{
+          elements: {
+            formButtonPrimary:
+              "bg-primary text-primary-foreground hover:bg-primary/90",
+            footerActionLink: "text-primary hover:text-primary/90",
+          },
+        }}
+        signInUrl="/sign-in"
+        fallbackRedirectUrl="/new-user"
+      />
+    </div>
+  );
 }
